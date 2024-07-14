1a61e76110ee3b44fd69d8af9e7509967684082f
Adil Dyer <adil@Adils-Macbook-Air.local>
2024-07-14 22:01:48 +0530

    secondcover in book club without refinement

---
+++ src/app/bookclub/components/SecondCover.js
@@ -0,0 +1,69 @@
+"use client";
+import React, { useState } from "react";
+import Link from "next/link";
+const SecondCover = ({ coverUrl }) => {
+  const images = [
+    "https://htmlcolorcodes.com/assets/images/colors/yellow-orange-color-solid-background-1920x1080.png",
+    "https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg",
+    "https://images.ctfassets.net/hrltx12pl8hq/28ECAQiPJZ78hxatLTa7Ts/2f695d869736ae3b0de3e56ceaca3958/free-nature-images.jpg?fit=fill&w=1200&h=630",
+    "https://img.freepik.com/premium-photo/house-with-pink-house-top_874813-1429.jpg",
+    "https://images.ctfassets.net/hrltx12pl8hq/28ECAQiPJZ78hxatLTa7Ts/2f695d869736ae3b0de3e56ceaca3958/free-nature-images.jpg?fit=fill&w=1200&h=630",
+  ];
+  const [selectedImage, setSelectedImage] = useState(images[0]);
+  const handleImageClick = (image) => {
+    setSelectedImage(image);
+  };
+
+  const handleCenterImageClick = () => {
+    // router.push(`/`);
+  };
+  return (
+    <div className="secondCover">
+      <img src={coverUrl} id="coverImg" alt="Cover Picture..." />
+      <div className="oneCardDiv">
+        <div className="announcements">
+          <div className="cardsAnnoun">
+            <Link href="#">
+              <div className="card">
+                <div className="imageDiv">
+                  <img src={selectedImage} alt="" />
+                </div>
+                <br />
+                <div className="textBody">
+                  <h5>Next Meeting :</h5>
+                  <p>Upper Audi, 23-June-2024</p>
+                </div>
+              </div>
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className="scroll-menu">
+        {images.map((image, index) => (
+          <div
+            onClick={() => handleImageClick(image)}
+            key={index}
+            className="announcements"
+          >
+            <div className="cardsAnnoun">
+              <Link href="#">
+                <div className="card">
+                  <div className="imageDiv">
+                    <img alt={`Thumbnail ${index}`} src={image} />
+                  </div>
+                  <br />
+                  <div className="textBody">
+                    <h5>Next Meeting :</h5>
+                    <p>Upper Audi, 23-June-2024</p>
+                  </div>
+                </div>
+              </Link>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default SecondCover;
--- src/app/bookclub/page.js
+++ src/app/bookclub/page.js
@@ -1,12 +1,17 @@
 import React from "react";
 import FirstCover from "./components/FirstCover";
+import SecondCover from "./components/SecondCover";
 const BookClub = () => {
-  
   return (
     <div className="bookClubDiv">
       <FirstCover
         coverUrl={
           "https://res.cloudinary.com/ddxv0iwcs/image/upload/v1720415865/bookclubcover_qxpmep.jpg"
+        }
+      />
+      <SecondCover
+        coverUrl={
+          "https://getwallpapers.com/wallpaper/full/7/a/b/1169002-full-size-black-and-white-check-wallpaper-1920x1080-for-desktop.jpg"
         }
       />
 
